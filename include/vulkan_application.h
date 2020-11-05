29061c6a013ee60af632868d987c738dfbfbf841
Sami Remes <sami.remes@gmail.com>
2020-11-05 21:23:33 +0200

    Formatting and updates

--- include/vulkan_application.h
+++ include/vulkan_application.h
@@ -10,13 +10,17 @@
 class VulkanRenderer : public QVulkanWindowRenderer
 {
   public:
-    VulkanRenderer(QVulkanWindow& window) : window_(window) {}
-    
-    void startNextFrame() override 
+    VulkanRenderer(QVulkanWindow& window)
+        : window_(window), device_functions_{nullptr}
+    {
+    }
+
+    void startNextFrame() override
     {
         auto command_buffer = window_.currentCommandBuffer();
         auto render_pass_info = GetRenderPassBeginInfo();
-        device_functions_->vkCmdBeginRenderPass(command_buffer, &render_pass_info, VK_SUBPASS_CONTENTS_INLINE);
+        device_functions_->vkCmdBeginRenderPass(
+            command_buffer, &render_pass_info, VK_SUBPASS_CONTENTS_INLINE);
         // @todo Draw everything here
         device_functions_->vkCmdEndRenderPass(command_buffer);
         window_.frameReady();
@@ -34,19 +38,24 @@ class VulkanRenderer : public QVulkanWindowRenderer
     {
         // @todo Init projection matrix etc. depending on window size here
     }
-  
+
   private:
     VkRenderPassBeginInfo GetRenderPassBeginInfo();
 
     QVulkanWindow& window_;
     QVulkanDeviceFunctions* device_functions_;
+
+    const VkClearValue clear_values_{{0.0F, 0.0F, 0.0F, 1.0F}};
 };
 
 class VulkanWindow : public QVulkanWindow
 {
   public:
-    QVulkanWindowRenderer* createRenderer() override { return new VulkanRenderer(*this); }
-  
+    QVulkanWindowRenderer* createRenderer() override
+    {
+        return new VulkanRenderer(*this);
+    }
+
   protected:
     void keyPressEvent(QKeyEvent* event) override { close(); }
 };
@@ -69,12 +78,13 @@ class VulkanApplication : public QGuiApplication
                                    << "VK_LAYER_GOOGLE_unique_objects");
         if (!vulkan_instance_.create())
         {
-            qFatal("Failed to create vulkan instance: %d", vulkan_instance_.errorCode());
+            qFatal("Failed to create vulkan instance: %d",
+                   vulkan_instance_.errorCode());
         }
         vulkan_window_.setVulkanInstance(&vulkan_instance_);
         vulkan_window_.showMaximized();
     }
-  
+
   private:
     QVulkanInstance vulkan_instance_;
     VulkanWindow vulkan_window_;
--- src/vulkan_application.cpp
+++ src/vulkan_application.cpp
@@ -6,14 +6,13 @@ VkRenderPassBeginInfo VulkanRenderer::GetRenderPassBeginInfo()
     render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
     render_pass_info.renderPass = window_.defaultRenderPass();
     render_pass_info.framebuffer = window_.currentFramebuffer();
-    
+
     const QSize size = window_.swapChainImageSize();
     render_pass_info.renderArea.extent.width = size.width();
     render_pass_info.renderArea.extent.height = size.height();
 
-    const VkClearValue clear_values = {0.0F, 0.0F, 0.1F, 1.0F};
     render_pass_info.clearValueCount = 1;
-    render_pass_info.pClearValues = &clear_values;
+    render_pass_info.pClearValues = &clear_values_;
 
     return render_pass_info;
 }
